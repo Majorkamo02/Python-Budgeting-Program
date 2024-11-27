7c29e84aa78ac7dcd4425bda845c9669562cc663
Majorkamo02 <cameron.l.jolly@gmail.com>
2024-11-27 02:50:44 -0700

    updating and rewriting relationships database connection

--- Classes/data_management.py
+++ Classes/data_management.py
@@ -1,16 +1,24 @@
 import sqlalchemy
-from .user import User, session
+from .user import User, Base
+from Data_Base.database import session
 
 class data_management(User):
-    def create_new_user(first, last, email):        
-        try:
-            new_user = User(first_name=first, last_name=last, email=email)
-            session.add(new_user)
-            session.commit()
-            print(f"User {new_user.first_name} {new_user.last_name} Created with account number: {new_user.account_number}")
-            return new_user
-        except sqlalchemy.exc.IntegrityError:
-            print("Error! Please try another email address")
+    def create_new_user():        
+        first = input("What is your first name? ")
+        last = input("What is your last name? ")
+        while True:    
+            email = input("What is your email? ")
+            
+            try:
+                new_user = User(first_name=first, last_name=last, email=email)
+                session.add(new_user)
+                session.commit()
+                print(f"User {new_user.first_name} {new_user.last_name} Created with account number: {new_user.id}")
+                return new_user
+                break
+            except sqlalchemy.exc.IntegrityError:
+                session.rollback()
+                print("Error! Please try another email address")
 
 
     def csv_commit(file):
@@ -23,4 +31,3 @@ class data_management(User):
         pass
 
 #test
-    
---
+++ Data_Base/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_engine(r"sqlite:///C:/Users\Cameron\Desktop\School\Fall 2024\Object Oriented Programming\Final Project\Data_Base/test.db")
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def init_db():
+    Base.metadata.create_all(engine)
