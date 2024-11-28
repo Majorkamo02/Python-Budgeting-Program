ec7fb13e4aa8952ad5090799234157d810b4b2bc
Majorkamo02 <cameron.l.jolly@gmail.com>
2024-11-28 00:30:41 -0700

    working password

--- Classes/main.py
+++ Classes/main.py
@@ -8,12 +8,10 @@ from database import init_db
 def main():
     init_db()
 
-    test_user2 = data_management.create_new_user()
-    # test_user = data_management.create_new_user("Cameron","Jolly","cameron.l.jolly@gmail.com")
-    # test_user3 = data_management.create_new_user("Parker","Thurston","parker.thurston@gmail.com")
+    # data_management.create_new_user()
+    current_user = data_management.login()
+    print(current_user)
 
-    # session.add(test_user2)
-    # session.commit()
 
 if __name__ == '__main__':
     main()
